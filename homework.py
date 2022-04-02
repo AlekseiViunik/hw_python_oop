9fe08863b63eed7ef074cd8ea75d576ac8aca8a7
Aleksei Viunik <vjunik88@gmail.com>
2022-04-02 19:00:08 +0300

    Homework is done (round 3 and hope it is the last one)

--- homework.py
+++ homework.py
@@ -1,5 +1,10 @@
-from dataclasses import dataclass, asdict
-from typing import Dict, Type, ClassVar
+from dataclasses import asdict, dataclass
+from typing import ClassVar, Dict, List, Type, Union
+
+TIKKER_SWIM: str = 'SWM'
+TIKKER_RUN: str = 'RUN'
+TIKKER_WALK: str = 'WLK'
+TYPES_LIST: List[str] = [TIKKER_SWIM, TIKKER_RUN, TIKKER_WALK]
 
 
 @dataclass
@@ -20,7 +25,7 @@ class InfoMessage:
 
     def get_message(self):
         """Метод, выводящий сообщение о тренировке."""
-        temp_dict: Dict[str, float] = asdict(self)
+        temp_dict: Dict[str, Union[float, str]] = asdict(self)
         return self.MESSAGE_CONST.format(**temp_dict)
 
 
@@ -49,7 +54,8 @@ class Training:
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        raise NotImplementedError('Methods of some classes are not redefined.')
+        raise NotImplementedError(f'Method of class {type(self).__name__} '
+                                  'is not redefined.')
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
@@ -69,8 +75,8 @@ class Running(Training):
 
     def get_spent_calories(self) -> float:
         return ((self.RUN_COEF_1 * self.get_mean_speed() - self.RUN_COEF_2)
-                * self.weight / super().M_IN_KM * self.duration
-                * super().MIN_IN_HOUR)
+                * self.weight / self.M_IN_KM * self.duration
+                * self.MIN_IN_HOUR)
 
 
 class SportsWalking(Training):
@@ -78,19 +84,21 @@ class SportsWalking(Training):
 
     WALK_COEF_1: ClassVar[float] = 0.035
     WALK_COEF_2: ClassVar[float] = 0.029
+    WALK_DEGREE: ClassVar[float] = 2.0
 
     def __init__(self,
                  action: int,
                  duration: float,
                  weight: float,
-                 height: int) -> None:
+                 height: int,
+                 ) -> None:
         super().__init__(action, duration, weight)
         self.height = height
 
     def get_spent_calories(self) -> float:
-        return ((self.WALK_COEF_1 * self.weight + (super().get_mean_speed()**2
-                // self.height) * self.WALK_COEF_2 * self.weight)
-                * self.duration * self.MIN_IN_HOUR)
+        return ((self.WALK_COEF_1 * self.weight + (self.get_mean_speed()
+                ** self.WALK_DEGREE // self.height) * self.WALK_COEF_2
+                * self.weight) * self.duration * self.MIN_IN_HOUR)
 
 
 class Swimming(Training):
@@ -105,30 +113,34 @@ class Swimming(Training):
                  duration: float,
                  weight: float,
                  length_pool: int,
-                 count_pool: int) -> None:
+                 count_pool: int,
+                 ) -> None:
         super().__init__(action, duration, weight)
 
         self.length_pool = length_pool
         self.count_pool = count_pool
 
     def get_mean_speed(self) -> float:
-        return (self.length_pool * self.count_pool / super().M_IN_KM
+        return (self.length_pool * self.count_pool / self.M_IN_KM
                 / self.duration)
 
     def get_spent_calories(self) -> float:
-        return ((self.get_mean_speed() + self.SWIM_COEF_1)
-                * self.SWOM_COEF_2
+        return ((self.get_mean_speed() + self.SWIM_COEF_1) * self.SWOM_COEF_2
                 * self.weight)
 
 
 def read_package(workout_type: str, data: list) -> Training:
     """Прочитать данные полученные от датчиков."""
     package: Dict[str, Type[Training]] = {
-        'SWM': Swimming,
-        'RUN': Running,
-        'WLK': SportsWalking,
+        TIKKER_SWIM: Swimming,
+        TIKKER_RUN: Running,
+        TIKKER_WALK: SportsWalking,
     }
-    return package[workout_type](*data)
+    if workout_type in TYPES_LIST:
+        return package[workout_type](*data)
+    else:
+        raise ValueError(f'You have entered {workout_type} as workout_type, '
+                         f'should be something from this list: {TYPES_LIST}')
 
 
 def main(training: Training) -> None:
@@ -136,10 +148,6 @@ def main(training: Training) -> None:
     info: InfoMessage = training.show_training_info()
     print(info.get_message())
 
-
-TIKKER_SWIM: str = 'SWM'
-TIKKER_RUN: str = 'RUN'
-TIKKER_WALK: str = 'WLK'
 
 if __name__ == '__main__':
     packages = [
